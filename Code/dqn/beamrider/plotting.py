d9dbbfb32cb979c68dc5f765fe5c2cedd5017ebd
Ted Xiao <ted@ml.berkeley.edu>
2017-05-08 18:28:39 -0700

    Adding more plotting results

--- Code/dqn/beamrider/plotting.py
+++ Code/dqn/beamrider/plotting.py
@@ -4,27 +4,26 @@ from matplotlib.backends.backend_pdf import PdfPages
 import pickle
 
 # Plotting for Q1, performance on Pong for lr=1 on images for 4.2m steps
-pixel_data = pickle.load(open('breakout_dqn_2000000_data.pkl','rb')) # lookahead
+pixel_data = pickle.load(open('beamrider_dqn_2200000_data.pkl','rb')) # lookahead
 pixel_t = pixel_data['t_log']
 pixel_mean_rewards = pixel_data['mean_reward_log']
 pixel_best_rewards = pixel_data['best_mean_log']
 
-lookahead_data = pickle.load(open('breakout_la_600000_data.pkl','rb')) # lookahead
-lookahead_t = lookahead_data['t_log']
-lookahead_mean_rewards = lookahead_data['mean_reward_log']
-lookahead_best_rewards = lookahead_data['best_mean_log']
+# lookahead_data = pickle.load(open('lr1.0_2800000_data.pkl','rb')) # lookahead
+# lookahead_t = lookahead_data['t_log']
+# lookahead_mean_rewards = lookahead_data['mean_reward_log']
+# lookahead_best_rewards = lookahead_data['best_mean_log']
 
 pixel_plot= plt.figure()
 pixel_mean_rew, = plt.plot(pixel_t, pixel_mean_rewards, label='DQN Mean 100-Episode Reward')
 pixel_best_rew, = plt.plot(pixel_t, pixel_best_rewards, label='DQN Best Mean Reward')
-la_mean_rew, = plt.plot(lookahead_t, lookahead_mean_rewards, label='Lookahead Mean 100-Episode Reward')
-la_best_rew, = plt.plot(lookahead_t, lookahead_best_rewards, label='Lookahead Best Mean Reward')
-plt.suptitle('DQN vs. DQN + Lookahead Performance on Breakout', fontsize=20)
+# la_mean_rew, = plt.plot(lookahead_t, lookahead_mean_rewards, label='Lookahead Mean 100-Episode Reward')
+# la_best_rew, = plt.plot(lookahead_t, lookahead_best_rewards, label='Lookahead Best Mean Reward')
+plt.suptitle('DQN vs. DQN + Lookahead Performance on Beamrider', fontsize=20)
 plt.xlabel('Timesteps')
 plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 plt.ylabel('Reward')
 plt.legend(loc=4)
-plt.show()
 pp = PdfPages('plot.pdf')
 pp.savefig(pixel_plot)
 pp.close()
